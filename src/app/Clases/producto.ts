5ab9f443eb7da08eac5df5cb72ae5389acab5c7a
IvonneDaniela <ivonne.cristancho2316@gmail.com>
2020-08-11 16:02:46 -0500

    ..

---
+++ src/app/Clases/producto.ts
@@ -0,0 +1,7 @@
+export class Producto{
+    userId: String;
+    id: String;
+    title: String;
+    body: String;
+
+}
--- src/app/components/producto/producto.component.ts
+++ src/app/components/producto/producto.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
+import { ProductoService } from 'src/app/services/producto.service';
+import { Producto } from 'src/app/Clases/producto';
 
 @Component({
   selector: 'app-producto',
@@ -8,22 +10,31 @@ import { ActivatedRoute } from "@angular/router";
 })
 export class ProductoComponent implements OnInit {
 
-  id: String
-  tienda: String
-  nombre: String
+  listadoProducto: Producto[];
+  postId: Producto;
 
-  constructor(private _route: ActivatedRoute) { }
+  ngOnInit() {
+    this.productoService.getProducto()
+    .subscribe(
+      (data) => {
+        this.listadoProducto = data;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
 
-  ngOnInit(): void {
-    this.id = this._route.snapshot.paramMap.get('id');
-    this.tienda = this._route.snapshot.paramMap.get('tienda');
-    this.nombre = this._route.snapshot.paramMap.get('nombre');
 
-    //debug
-    console.log(this.id);
-    console.log(this.tienda);
-    console.log(this.nombre);
-    //
+
+    var post = new Producto();
+    this.productoService.postProducto(post)
+    .subscribe(
+      (data) =>{
+        this.postId.id = post.id;
+      }
+    )
   }
+
+  constructor(private route: ActivatedRoute, private productoService: ProductoService) { }
 
 }
