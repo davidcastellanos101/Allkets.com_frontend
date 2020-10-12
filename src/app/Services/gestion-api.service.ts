c0bdaca18e29a976e99bc14c9503941dcdbe7c23
alexander-ds <alxds.ad@gmail.com>
2020-10-11 21:13:53 -0500

    Declaracion de contantes de direccion y puerto en servicio

--- src/app/Services/gestion-api.service.ts
+++ src/app/Services/gestion-api.service.ts
@@ -1,88 +1,108 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { TiendaDTO } from '../Clases/TiendaDTO';
 import { Producto } from '../Clases/producto';
 
+const DIRECCION = 'http://167.172.114.200';
+const PUERTO = '30126';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
+
 export class GestionAPIService {
+  constructor(protected httpClient: HttpClient) {}
 
-  constructor(protected httpClient: HttpClient) { }
-  
-  public obtenerProducto(id: any): Observable<any>{
-    let idJson : string = JSON.stringify(id);
+  public obtenerProducto(id: any): Observable<any> {
+    let idJson: string = JSON.stringify(id);
     let headers = new HttpHeaders();
     /* cabecera que nos da el tipo de archivo que vamos a llamar */
-    headers = headers.append('Content-Type','application/json');
-    
+    headers = headers.append('Content-Type', 'application/json');
+
     /* cabecera  para algun token , falta token
     headers.set('Authorization','bearer');
     */
 
-    return this.httpClient.post('http://167.172.114.200:30126/getProducto', idJson, {headers});
-    
+    return this.httpClient.post(
+      DIRECCION + ':' + PUERTO + '/getProducto',
+      idJson,
+      { headers }
+    );
   }
 
-  public obtenerTienda(id: any): Observable<any>{
-    let idJson : string = JSON.stringify(id);
+  public obtenerTienda(id: any): Observable<any> {
+    let idJson: string = JSON.stringify(id);
     let headers = new HttpHeaders();
     /* cabecera que nos da el tipo de archivo que vamos a llamar */
-    headers = headers.append('Content-Type','application/json');
+    headers = headers.append('Content-Type', 'application/json');
     /* cabecera  para algun token , falta token
     headers.set('Authorization','bearer');
     */
-    return this.httpClient.post('http://167.172.114.200:30126/getTienda', idJson, {headers});
-    
+    return this.httpClient.post(
+      DIRECCION + ':' + PUERTO + '/getTienda',
+      idJson,
+      { headers }
+    );
   }
 
-  public obtenerProductos(): Observable<any>{
-
-    return this.httpClient.post('http://167.172.114.200:30126/getProductos','');
-    
+  public obtenerProductos(): Observable<any> {
+    return this.httpClient.post(DIRECCION + ':' + PUERTO + '/getProductos', '');
   }
 
-  public obtenerProductosTienda2(tienda: String): Observable<any>{
-
-    return this.httpClient.post('http://167.172.114.200:30126/getProductosByTienda?tienda='+tienda,'');
-    
+  public obtenerProductosTienda2(tienda: String): Observable<any> {
+    return this.httpClient.post(
+      DIRECCION + ':' + PUERTO + '/getProductosByTienda?tienda=' + tienda,
+      ''
+    );
   }
 
-  // metodos de login y logout  usuario asi como la creacion 
+  // metodos de login y logout  usuario asi como la creacion
 
-  public static logout(){
-    localStorage.removeItem("token");
-    localStorage.removeItem("user");
+  public static logout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
   }
 
-  public static islogged(): boolean{
-    if (localStorage.getItem("token") != undefined && localStorage.getItem("user") != undefined) {
+  public static islogged(): boolean {
+    if (
+      localStorage.getItem('token') != undefined &&
+      localStorage.getItem('user') != undefined
+    ) {
       return true;
     }
     return false;
   }
 
-  public obtenerTokenUsuario(usuario: string,passwd: string ): Observable<any>{
+  public obtenerTokenUsuario(usuario: string, passwd: string): Observable<any> {
     let request = {
-      "usuario":usuario,
-      "passwd":passwd
+      usuario: usuario,
+      passwd: passwd,
     };
-    
-    let requestJSON : string = JSON.stringify(request);
+
+    let requestJSON: string = JSON.stringify(request);
     let headers = new HttpHeaders();
     /* cabecera que nos da el tipo de archivo que vamos a llamar */
-    headers.set('Content-Type','application/json');
-    
-    let tokentmp = this.httpClient.post('http://167.172.114.200:30126/user?user='+usuario+'&password='+passwd, '');
+    headers.set('Content-Type', 'application/json');
+
+    let tokentmp = this.httpClient.post(
+      DIRECCION +
+        ':' +
+        PUERTO +
+        '/user?user=' +
+        usuario +
+        '&password=' +
+        passwd,
+      ''
+    );
     tokentmp.subscribe(
       (data) => {
-        localStorage.setItem("token", Object(data)["token"]);
-        localStorage.setItem("user", Object(data)["id"]);
-        console.log(localStorage.getItem("token"));
+        localStorage.setItem('token', Object(data)['token']);
+        localStorage.setItem('user', Object(data)['id']);
+        console.log(localStorage.getItem('token'));
         console.log(JSON.stringify(data));
-        if (data == undefined){
-          alert('Login fallido, revisa los datos ingresados')
+        if (data == undefined) {
+          alert('Login fallido, revisa los datos ingresados');
         }
       },
       (error) => {
@@ -90,49 +110,55 @@ export class GestionAPIService {
       }
     );
     return tokentmp;
-    
   }
-  
-  public crearTienda(tienda: TiendaDTO): Observable<any>{
-    let objJson : string = JSON.stringify(tienda);
+
+  public crearTienda(tienda: TiendaDTO): Observable<any> {
+    let objJson: string = JSON.stringify(tienda);
     let headers = new HttpHeaders();
     /* cabecera que nos da el tipo de archivo que vamos a llamar */
-    headers = headers.append('Content-Type','application/json');
-    
-    headers = headers.append('Authorization',localStorage.getItem("token"));
+    headers = headers.append('Content-Type', 'application/json');
+
+    headers = headers.append('Authorization', localStorage.getItem('token'));
     headers = headers.append('Access-Control-Allow-Origin', '*');
 
-    return this.httpClient.post('http://167.172.114.200:30126/createTienda', objJson, {headers});
-    
+    return this.httpClient.post(
+      DIRECCION + ':' + PUERTO + '/createTienda',
+      objJson,
+      { headers }
+    );
   }
 
-  /* consulta y creacion de producto  cambio hecho por alx*/ 
+  /* consulta y creacion de producto  cambio hecho por alx*/
 
-  public consulta(id: any): Observable<any>{
-    let idJson : string = JSON.stringify(id);
+  public consulta(id: any): Observable<any> {
+    let idJson: string = JSON.stringify(id);
     let headers = new HttpHeaders();
     /* cabecera que nos da el tipo de archivo que vamos a llamar */
-    headers.set('Content-Type','application/json');
-    
+    headers.set('Content-Type', 'application/json');
+
     /* cabecera  para algun token , falta token
     headers.set('Authorization','bearer');
     */
 
-    return this.httpClient.post('http://167.172.114.200:30126/getProducto', idJson);
-    
+    return this.httpClient.post(
+      DIRECCION + ':' + PUERTO + '/getProducto',
+      idJson
+    );
   }
 
-  public crearProducto(pdto: Producto): Observable<any>{
-    let idJson : string = JSON.stringify(pdto);
+  public crearProducto(pdto: Producto): Observable<any> {
+    let idJson: string = JSON.stringify(pdto);
     let headers = new HttpHeaders();
     /* cabecera que nos da el tipo de archivo que vamos a llamar */
-    headers = headers.append('Content-Type','application/json');
-    
-    headers = headers.append('Authorization',localStorage.getItem("token"));
+    headers = headers.append('Content-Type', 'application/json');
+
+    headers = headers.append('Authorization', localStorage.getItem('token'));
     headers = headers.append('Access-Control-Allow-Origin', '*');
 
-    return this.httpClient.post('http://167.172.114.200:30126/createProducto', idJson, {headers});
-    
+    return this.httpClient.post(
+      DIRECCION + ':' + PUERTO + '/createProducto',
+      idJson,
+      { headers }
+    );
   }
 }
-
--- src/app/components/promos/promos.component.ts
+++ src/app/components/promos/promos.component.ts
@@ -5,36 +5,33 @@ import { GestionAPIService } from 'src/app/Services/gestion-api.service';
 @Component({
   selector: 'app-promos',
   templateUrl: './promos.component.html',
-  styleUrls: ['./promos.component.scss']
+  styleUrls: ['./promos.component.scss'],
 })
 export class PromosComponent implements OnInit {
-
   public productos: Producto[];
   @Input() public tienda: string;
 
-  constructor(private gestionAPI: GestionAPIService) { }
+  constructor(private gestionAPI: GestionAPIService) {}
 
   ngOnInit(): void {
-
-    if (this.tienda != null){
-    this.gestionAPI.obtenerProductosTienda2(this.tienda).subscribe(
-      (data) => {
-        this.productos = data;
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  }
-  else{
-    this.gestionAPI.obtenerProductos().subscribe(
-      (data) => {
-        this.productos = data;
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  }
+    if (this.tienda != null) {
+      this.gestionAPI.obtenerProductosTienda2(this.tienda).subscribe(
+        (data) => {
+          this.productos = data;
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+    } else {
+      this.gestionAPI.obtenerProductos().subscribe(
+        (data) => {
+          this.productos = data;
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+    }
   }
 }
